b9b4575d108b55d5e2871d72ce79692b524e823d
leticia_jesus <leticia_jesus@10.1.48.225>
2013-07-31 16:20:33 -0300

    Herdando da classe Transportadora.

---
+++ src/br/senai/sc/model/negocio/Transportadora.java
@@ -0,0 +1,32 @@
+package br.senai.sc.model.negocio;
+
+/**
+ * Classe que representa as transportadoras do software.
+ *
+ * @version 1.0 31/07/13
+ * @author leticia_jesus
+ */
+public class Transportadora extends PessoaJuridica {
+
+    private String tipoTransporte;
+    private double precoFrete;
+
+    public Transportadora() {
+    }
+
+    public String getTipoTransporte() {
+        return tipoTransporte;
+    }
+
+    public void setTipoTransporte(String tipoTransporte) {
+        this.tipoTransporte = tipoTransporte;
+    }
+
+    public double getPrecoFrete() {
+        return precoFrete;
+    }
+
+    public void setPrecoFrete(double precoFrete) {
+        this.precoFrete = precoFrete;
+    }
+}
