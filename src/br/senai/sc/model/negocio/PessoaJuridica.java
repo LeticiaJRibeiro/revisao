9f9b2f822b022911a25180d90d4be20e5af1d294
leticia_jesus <leticia_jesus@10.1.48.225>
2013-07-31 15:49:33 -0300

    Adicionando a classe PessoaJuridica

---
+++ src/br/senai/sc/model/negocio/PessoaJuridica.java
@@ -0,0 +1,44 @@
+
+package br.senai.sc.model.negocio;
+
+/**
+ * Classe que representa as pessoas jurídicas do sistema
+ *@version 1.0 31/07/13
+ * @author leticia_jesus
+ */
+public class PessoaJuridica {
+    
+    private String cnpj;
+    private String razaoSocial;
+    private String nomeFantasia;
+
+    public PessoaJuridica() {
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+    
+    
+    
+}
