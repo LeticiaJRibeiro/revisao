edd8d150b8889d1d2c2114c4b2d70746960884f6
leticia_jesus <leticia_jesus@10.1.48.220>
2013-08-12 16:21:00 -0300

    onde foram setados os valores

--- src/br/senai/sc/view/JanelaPrincipal.java
+++ src/br/senai/sc/view/JanelaPrincipal.java
@@ -1,41 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package br.senai.sc.view;
 
 import br.senai.sc.model.negocio.Funcionario;
-import br.senai.sc.model.persistencia.FuncionarioDAO;
+import br.senai.sc.model.negocio.Produto;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
- * Classe que controla toda a parte visual da aplicação.
+ * Classe que controla toda a parte visual da aplicação
  *
- * @version 1.0 31/07/13
+ * @version 1.0 31/07/2013
  * @author leticia_jesus
  */
 public class JanelaPrincipal {
 
     public static void main(String[] args) {
-     List<Funcionario> funcionarios = new ArrayList<>();
-    //funcionários cadastrados
-        mostraMenu(funcionarios);
+
+
+        // Vetor dinamico para armazenar funcionarios cadastrados
+        List<Funcionario> funcionarios = new ArrayList<>();
+
+        //Vetor dinamico para armazenar produtos cadastrados
+        List<Produto> produtos = new ArrayList<>();
+
+        mostraMenu(funcionarios, produtos);
+
     }
 
-    public static void mostraMenu(List<Funcionario>funcionarios) {
+    public static void mostraMenu(List<Funcionario> funcionarios, List<Produto> produtos) {
+
         JanelaFuncionario jf = new JanelaFuncionario();
+
+        JanelaProduto jp = new JanelaProduto();
+
         int opcao = 0;
         do {
-            opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a opção:"
-                    + "\n1 -funcionario"
-                    + "\n2-cliente"
-                    + "\n3-fornecedor"
-                    + "\n4-transportadora"
-                    +"\n5-Produto"
-                    + "\n6-Sair"));
+
+            opcao = Integer.parseInt(JOptionPane.showInputDialog("Informe a opção: "
+                    + "\n1 - Funcionario: "
+                    + "\n2 - Produto: "
+                    + "\n3 - Fornecedor"
+                    + "\n4 - Transportadora"
+                    + "\n5 - Sair"));
+
             switch (opcao) {
                 case 1:
-                   jf.menuFuncionario(funcionarios);
+                    jf.menuFuncionario(funcionarios);
                     break;
                 case 2:
+                    jp.menuProduto(produtos);
                     break;
                 case 3:
                     break;
@@ -43,8 +60,6 @@ public class JanelaPrincipal {
                     break;
                 default:
                     JOptionPane.showMessageDialog(null, "Opção inexistente");
-
-
             }
         } while (opcao != 0);
 
